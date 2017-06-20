4725f2378eeef171a0ebcb16786dac3c641ae108
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-06-20 16:54:54 +0100

    login and profile pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import *
+from .views import logout, profile, login
 
 urlpatterns = [
     url(r'^logout', logout, name='logout'),
+    url(r'^login', login, name='login'),
+    url(r'^profile', profile, name='profile'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages, auth
+from .forms import UserRegistrationForm, UserLoginForm, SubscribeForm
+from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -8,3 +11,35 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(reverse('index'))
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    return render(request, 'profile.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST.get('username_or_email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and 'next' in request.GET:
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
+
+
